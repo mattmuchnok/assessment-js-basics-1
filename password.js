a5d75dc27361f4eaacff1a346b20297bcce161fb
mattmuchnok <mattmuchnok@gmail.com>
2022-01-21 14:12:50 -0500

    added 3 additional password constraints

--- password.js
+++ password.js
@@ -10,15 +10,28 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
+// Global Variables:
+
+
 // Welcome the user to the password validator tool
 console.log(`*** Welcome to the Password Validator Tool! ***`)
 
 // Prompt the user for a password to validate
 reader.question(`Please enter the password you want to validate:`, function(input){
     
+    // Variables
+    const regex = /\d/;
+    const doesItHaveNumber = regex.test(input)
+
+    const regex2 = /[a-zA-Z]/
+    const doesItHaveLetter = regex2.test(input)
+
     // Check if the user's password is at least 10 characters long
-    if (input.length >= 10) {
-        // If the user's password meets the constraint, show a success message to the user
+    // ALSO, check if the user's password is less or equal to 20 characters long
+    // ALSO, check if the user's password contains numbers
+    // ALSO, check if the user's password contains letters
+    if (input.length >= 10 && input.length <= 20 && doesItHaveNumber === true && doesItHaveLetter === true) {
+        // If the user's password meets the constraints, show a success message to the user
         console.log(`SUCCESS!`)
     } else {
         // If the user's password fails the constraint, show a failure message to the user
